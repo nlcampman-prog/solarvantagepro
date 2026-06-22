f263340db152d6fac241042fc5e19c6363278795
AutoPilot Team <team@autopilotsales.org>
2026-06-22 20:41:15 +0000

    Fix build: missing components, CTABox frontmatter, RSS types, add 4 pages

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ export async function GET(context) {
   const articles = Object.values(import.meta.glob('../content/articles/*.mdx', { eager: true }));
   const items = articles
     .sort((a, b) => new Date(b.frontmatter?.pubDate || b.frontmatter?.date || 0).getTime() - new Date(a.frontmatter?.pubDate || a.frontmatter?.date || 0).getTime())
-    .map((article: any) => {
+    .map((article) => {
       const fm = article.frontmatter || {};
       const slug = (article.file?.split('/')?.pop()?.replace(/\.(mdx|md)$/, '')) || '';
       return { title: fm.title, description: fm.metaDescription || fm.description, pubDate: new Date(fm.pubDate || fm.date || Date.now()), link: `/articles/${slug}/` };
