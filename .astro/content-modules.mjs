09a1189e54b06fd383830e475e51ba21708e2dce
AutoPilot Team <team@autopilotsales.org>
2026-06-22 20:41:35 +0000

    Add .astro to gitignore

--- .astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/articles/best-solar-generators-2026.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fbest-solar-generators-2026.mdx&astroContentModuleFlag=true")]]);
-		
